ef9bbf833f0c75ffa5658c31fc9eb179f3eabb94
Ingrid Sanchez <jeenesan@gmail.com>
2020-10-29 10:17:42 +0100

    example

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 def add(a, b):
-    return a - b
+    return a + b
 
 
 def test_add():
@@ -7,4 +7,5 @@ def test_add():
     assert add('space', 'ship') == 'spaceship'
 
 
-
+def subtract(a, b):
+    return a + b  # <--- fix this in step 8
